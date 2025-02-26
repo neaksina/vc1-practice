60e6e52653bb2ee7b89325a2223d42898648d7fc
sina <neaksina752@gmail.com>
2025-02-26 11:19:55 +0700

    chage

--- Router/route.php
+++ Router/route.php
@@ -2,10 +2,10 @@
 require_once "Router.php";
 require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
-require_once "Controllers/WelcomeController.php";
+ 
 
 
 $route = new Router();
 $route->get("/", [WelcomeController::class, 'welcome']);
-
+ 
 $route->route();
